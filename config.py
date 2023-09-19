53887243a04938781afe44b579c3bc4c6b3700db
wongkiongho <125861978+wongkiongho@users.noreply.github.com>
2023-09-19 14:48:15 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cgbw848jqsd7.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "internship"
+custombucket = "internship-website"
+customregion = "us-east-1"
+
